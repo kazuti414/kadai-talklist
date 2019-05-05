2fdbb8feea2ac0d980fe4cbd8106b37fc7d2c889
kazuma.furui <kikekukukazm@gmail.com>
2019-05-05 14:27:46 +0000

    update fixied

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -30,8 +30,9 @@ class TasksController < ApplicationController
     end
     
     def update
+        
 
-        if current_user.tasks.update(task_params)
+        if @task.update(task_params)
             flash[:success] = 'タスクは正常に更新されました'
             redirect_to @task
         else
